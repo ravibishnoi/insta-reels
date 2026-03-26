8ef18850865ea1efec796f399aa5b156181aaf4d
RaviBishnoi <rbk4997@gmail.com>
2026-03-26 22:37:38 +0530

    first commit

---
+++ insta-reels/insta_reelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  insta_reelsApp.swift
+//  insta-reels
+//
+//  Created by Ravi Bishnoi on 26/03/26.
+//
+
+import SwiftUI
+
+@main
+struct insta_reelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
